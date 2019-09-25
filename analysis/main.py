6967499c356e8d695925a8e75ff01a63eef22292
Santiago Basanez <santiagobasanez@Santiagos-MBP-2.lan>
2019-09-25 19:59:42 -0300

    update

--- analysis/main.py
+++ analysis/main.py
@@ -1,71 +1,44 @@
 import matplotlib.pyplot as plt 
+import numpy as np
 
 import entropy_analysis as ea
 from fit_analysis import fix_size, fix_lamda
 from tools import make_list_files
-
-
-
-def same_curve_plots(schema_file, schema_label, N, min_lamda, max_lamda):
-	s = []
-	labels = []
-	for i in range(min_lamda, max_lamda+1):
-		entropy, max_entropy = ea.calculate_entropy(schema_file.format(i, N))
-		s.append(entropy)
-		labels.append(schema_label.format(i))
-
-	ea.plot_entropy(s, labels, max_entropy, title = 'Entropy')
-
-#schema_file = 'lamda_cm_poisson_{}_{}'
-#schema_label = 'Lambda = {}'
-#same_curve_plots(schema_file, schema_label, 10000, 4, 10)
-
-
-
-s = []
-labels = []
+from plots import same_curve_plots_fix_N, same_curve_plots_fix_L
 
 
 
 
-entropy, max_entropy = ea.calculate_entropy('tree_2_13')
-s.append(entropy)
-labels.append('Tree(3, 9)')
 
-entropy, max_entropy = ea.calculate_entropy('tree_3_9')
-s.append(entropy)
-labels.append('Tree(2,13) ')
+plt.figure(1)
 
-entropy, max_entropy = ea.calculate_entropy('lamda_cm_poisson_7_10000')
-s.append(entropy)
-labels.append('Configuration: Poisson distribution(10000, 7)')
+#same_curve_plots_fix_L('cm_poisson_{}_{}', 'N={}', 10, 1000, 19000)
 """
-entropy, max_entropy = ea.calculate_entropy('cm_equi_1_15_9000')
+s = []
+labels=[]
+"""
+s = []
+labels=[]
+entropy, max_entropy = ea.calculate_entropy('05_pt_er_10000')
 s.append(entropy)
-labels.append('Configuration: Unif-distribution(9000, 1,15)')
+label = 'BA(1, 10000)'
+labels.append(label)
+ea.plot_entropy([entropy], [label],max_entropy, pos=111)
+"""
 
-entropy, max_entropy = ea.calculate_entropy('erdos_renyi_10000')
+entropy, max_entropy = ea.calculate_entropy('bar_3_10000')
 s.append(entropy)
-labels.append('Erdos-Reny(10000, 0.0001)')
+label = 'BA(3, 10000)'
+labels.append(label)
+ea.plot_entropy([entropy], [label],max_entropy, pos=312)
 
 entropy, max_entropy = ea.calculate_entropy('bar_5_10000')
 s.append(entropy)
-labels.append('Barabasi(10000, 3)')
-"""
-ea.plot_entropy(s, labels, max_entropy, title = 'Entropy curves to diferent models')
+label = 'BA(5, 10000)'
+labels.append(label)
+ea.plot_entropy([entropy], [label],max_entropy, pos=313)
 
-#schema_file = 'fix_N_cm_poisson_{}_8000'
-#fix_size(schema_file, 3, 9, 1, 12000)
+plt.suptitle(f'Barabasi-Albert')
 """
-[ 0.09932262 -0.29348214]
-[[ 3.85566363e-06 -2.89174772e-05]
- [-2.89174772e-05  2.37123314e-04]]"""
 
-
-#schema_file = 'cm_poisson_7_{}'
-#fix_lamda (schema_file, 4000, 10000, 1000, 7)
-"""
-[p, m, b]
-[[ 0.0004086  -0.00050421  0.00053431]
- [-0.00050421  0.00064202 -0.00067142]
- [ 0.00053431 -0.00067142  0.00070644]]"""
+plt.show()
